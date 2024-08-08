a5173d5c04753299ab1a40c0dcb85f7481c2177c
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-08-08 10:19:18 -0500

    Created the nav bar and added styling and logic to adjust the side nav to the size of the screen, making it mobile friendly

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,7 +8,13 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const SideBar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  }
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-black text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-black text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -19,7 +25,7 @@ const SideBar = () => {
       {activeMenu && ( <>
         <div className='flex justify-between
           items-center'>
-          <Link to='/' onClick={() => setActiveMenu(false)}
+          <Link to='/' onClick={handleCloseSideBar}
             className='items-center gap-3 ml-3
             mt-4 flex text-xl font-extrabold
             tracking-tight dark:text-white
@@ -46,7 +52,7 @@ const SideBar = () => {
                 <NavLink
                   to={`/${link.name}`}
                   key={link.name}
-                  onClick={() => {}}
+                  onClick={handleCloseSideBar}
                   className={({ isActive }) => 
                     isActive ? activeLink : normalLink }
                 >
