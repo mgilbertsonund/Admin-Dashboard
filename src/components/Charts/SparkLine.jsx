fb87dbd9f55f49ae664c7dd6c979ca69db2df6b8
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-08-15 12:28:17 -0500

    Created Sparkline for the Ecommerce dashboard

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparkLine = () => {
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  console.log(data);
+  if (!data || data.length === 0) {
+    return null; // Return null or a loading indicator if data is not available
+  }
+
   return (
-    <div>
-      Spark
-    </div>
-  )
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType='Numeric'
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName='x'
+      yName='yval'
+      type={type}
+      tooltipSettings={{ 
+        visible: true,
+        format: '${x} : data ${yval}',
+        trackLineSettings: {
+          visible: true
+        }
+       }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 }
 
-export default SparkLine
+export default SparkLine;
