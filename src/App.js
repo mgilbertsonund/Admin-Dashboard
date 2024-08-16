a846db6c2fea2469e80eea42942fea939fb35036
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-08-15 20:29:27 -0500

    Theme settings are set up, but need to be added to context provider

--- src/App.js
+++ src/App.js
@@ -51,6 +51,7 @@ const App = () => {
                         </div>
                     
                     <div>
+                        <ThemeSettings />
                         <Routes>
                             {/* Dashboard */}
                             <Route 
